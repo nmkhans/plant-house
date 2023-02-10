f9559a624b2e2a9b8aa2a6e121f6a6442d1f271d
nmkhans <nurmoin05@gmail.com>
2023-02-10 11:49:35 +0600

    daisyui installed and configured

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,8 +4,18 @@ module.exports = {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
-  theme: {
-    extend: {},
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#a991f7",
+          secondary: "#f6d860",
+          accent: "#37cdbe",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      }
+    ],
   },
-  plugins: [],
 }

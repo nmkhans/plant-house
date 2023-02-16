87a5a55029434c8b345eeb102c7c2831d5169afb
nmkhans <nurmoin05@gmail.com>
2023-02-16 21:49:47 +0600

    product page issue fixed

--- src/pages/ProductDetail/ProductDetail.jsx
+++ src/pages/ProductDetail/ProductDetail.jsx
@@ -4,13 +4,14 @@ import { useParams } from 'react-router-dom';
 import capitalize from './../../utils/captalize';
 import { useDispatch } from 'react-redux';
 import { addToCart } from '../../redux/state/cartSlice';
+import Spinner from './../../components/Spinner/Spinner';
 
 const ProductDetail = () => {
     const { id } = useParams()
     const { data, isLoading } = useGetSingleProductQuery(id)
     const dispatch = useDispatch()
 
-    if (isLoading) return "loading..."
+    if (isLoading) return <Spinner />
 
     const product = data?.data[0]
     console.log(product)
@@ -21,7 +22,6 @@ const ProductDetail = () => {
                 <div className="card lg:card-side bg-base-100 shadow-xl border">
                     <figure>
                         <img className="w-[500px]" src={product.image} alt="Product image" />
-                        
                     </figure>
                     <div className="p-10">
                         <h2 className="text-2xl font-semibold text-base-200 mb-4">Name: {product.name}</h2>
@@ -31,9 +31,9 @@ const ProductDetail = () => {
                         <p className="text-base-200 mt-2">Stock: {product.stock ? product.stock + " pice" : <span className="text-red-500">Out of stock</span>}</p>
                         <div className="card-actions justify-start mt-10">
                             <button
-                            disabled={product.stock === 0}
-                            onClick={() => dispatch(addToCart({_id: product._id}))}
-                            className="btn btn-primary text-white w-full">
+                                disabled={product.stock === 0}
+                                onClick={() => dispatch(addToCart({ _id: product._id, name: product.name, image: product.image, price: product.price }))}
+                                className="btn btn-primary text-white w-full">
                                 Add to cart
                             </button>
                         </div>

0d54ce72c618c6f966a6f997d0ddce5891951716
nmkhans <nurmoin05@gmail.com>
2023-02-28 23:55:49 +0600

    site live

--- src/redux/api/api.js
+++ src/redux/api/api.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
 
 export const api = createApi({
     reducerPath: "api",
-    baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000/api/v1" }),
+    baseQuery: fetchBaseQuery({ baseUrl: "https://nmk-plant-house-backend.onrender.com/api/v1" }),
     tagTypes: ["category", "products", "order", "user", "podcast"],
     endpoints: (builder) => ({
         registerUser: builder.mutation({
